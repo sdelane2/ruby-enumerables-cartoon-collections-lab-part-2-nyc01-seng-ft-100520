d0c5550cf2b394f6a7872bd02960a012f48ed36a
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-22 21:41:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   planeteer_calls.map do |element|
     puts "#{element.capitalize}!"
-  
+  end
 end
 
 def long_planeteer_calls(planeteer_calls)
